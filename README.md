48fdf132cb382b3a92af275d35aa4fc034660e35
Lessio2006 <optuna2006@outlook.com>
2026-03-18 02:02:12 +0300

    Изменён project: -Добавлен Dockerfile -Добавлен docker-compose.yml (контеёнер поднят)                                 - Изменён README.md (добавлено описание работы с проектом)             - Изменён order.py (написаны docstrings к функциям)                  - Изменён main.py (добавлен хост 0.0.0.0)

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
 # project
 
 
+### Предварительные требования
+- Docker и Docker Compose
+- Git
+
+# Запуск
 ```bash
 python -m venv venv
 ```
 
-#Установите зависимости
-```pip install -r requirements.txt```
+# Запустить с Docker Compose:
+```bash
+docker-compose up --build
+```
+
+# Documentation API (Swagger UI)
+```http://localhost:8000/docs```
+
+## 🐳 Docker
+
+Сборка и запуск в контейнерах:
+```bash
+# Сборка образов
+docker-compose build
+
+# Запуск в фоне
+docker-compose up -d
+
+# Просмотр логов
+docker-compose logs -f
+
+# Остановка
+docker-compose down
+```
+
+## 🗄 Структура БД
+
+Основные таблицы:
+- `payments` - платежи
+- `invoices` - счета
+- `orders` - заказы
+- `clients` - клиенты
+- `comments` - отзывы
+
 
 
 
--- src/routers/order.py
+++ src/routers/order.py
@@ -1,6 +1,5 @@
 from src.db_connection import Database
 from fastapi import APIRouter
-import os
 from .schemas import OrderSchema
 from .utils import add_order
 
@@ -10,13 +9,34 @@ router = APIRouter()
 
 @router.post("/orders/pay", tags=["Заказ товара 🛒"])
 def set_order(order_data: OrderSchema):
-    """Оформляет заказ товара"""
+    """Оформляет заказ товара
+        Создает запись о заказе в системе и инициирует процесс оплаты.
+    Args:
+        order_data (OrderSchema): Данные заказа, включая:
+            - client_id: ID клиента
+            - product: наименование товара
+            - quantity: количество
+            - order_amount: сумма заказа
+    Returns:
+        dict: Статус операции и сообщение о результате
+    Raises:
+        HTTPException(400): При некорректных данных заказа
+        HTTPException(500): При ошибке сервера базы данных"""
     add_order(order_data)
 
 
 @router.get("/orders/pay/{order_id}/client/{client_id}", tags=["Заказ товара 🛒"])
 def get_order(order_id: int, client_id: int):
-    """Показывает статус оплаты"""
+    """Получает статус оплаты заказа для конкретного клиента.
+    Проверяет текущий статус платежа по идентификатору заказа и клиента.
+    Args:
+        order_id (int): Уникальный идентификатор заказа в системе
+        client_id (int): Уникальный идентификатор клиента
+    Returns:
+        dict: Хэш тэйбл со статусо и сообщением
+    Raises:
+        HTTPException(404): Если заказ или клиент не найдены
+        HTTPException(500): При ошибке подключения к базе данных"""
     Database.initialize()
     conn = Database().get_connection()
     try:
@@ -30,6 +50,9 @@ def get_order(order_id: int, client_id: int):
 
 @router.delete("/orders/progress/{order_id}", tags=["Возврат товара ↩️"])
 def delete_order(order_id: int):
+    """Оформляет возврат товара
+    Args: order_id (int): Уникальный идентификатор заказа
+    Returns: dict: Хэш тэбл со статусом и сообщением"""
     Database.initialize()
     conn = Database().get_connection()
     try:
@@ -43,13 +66,20 @@ def delete_order(order_id: int):
 
 
 @router.put("/reviews/{comment_id}", tags=["Отзывы 💬"])
-def update_comment(ratio: str, ratio_val:int, comment_id: int):
+def update_comment(column_name: str, ratio_val:int, comment_id: int):
+    """Обновляет комментарий пользователя
+    Args:
+        column_name (str): Имя поля для обновления. Допустимые значения:
+        ratio_val (int): - "ratio": оценка товара (например, от 1 до 5)
+        comment_id (int): Уникальный идентификатор комментария
+    Returns:
+        dict: Объект со статусом и сообщением"""
     Database.initialize()
     conn = Database().get_connection()
     try:
         with conn.cursor() as cursor:
             cursor.execute("SET search_path TO tqm")
-            cursor.execute(f'''UPDATE comments SET {ratio} = %s WHERE comment_id = %s''', (ratio_val, comment_id,))
+            cursor.execute(f'''UPDATE comments SET {column_name} = %s WHERE comment_id = %s''', (ratio_val, comment_id,))
             return {'status': 'ok', 'message': 'comment updated successfully'}
     except Exception as e:
         return {'status': 'error', 'message': str(e)}
